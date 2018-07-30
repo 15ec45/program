eba20667c3c631e85868adbcd8e9efd4d25249e5
15ec45 <priyasengathir19@gmail.com>
2018-07-30 15:45:10 +0530

    Create one to ten.py

---
+++ one to ten.py
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class number
+{
+public static void main(String []args)
+{
+int a;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+if(a<11)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
