e62f0076a199c8415b21e36662f522a1b4acad88
15ec45 <priyasengathir19@gmail.com>
2018-07-30 16:16:33 +0530

    Create nextno.java

---
+++ next no/nextno.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+public class nextno
+{
+public static void main(String []args)
+{
+int a;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+{
+a=a+1;
+}
+System.out.println(a);
+}
+}
