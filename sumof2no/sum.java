1eafb726c45d987c26cd0c673619ad59d1579f47
15ec45 <priyasengathir19@gmail.com>
2018-07-30 16:28:34 +0530

    Create sum.java

---
+++ sumof2no/sum.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class sum
+{
+public static void main(String []args)
+{
+int a,b;
+Scanner s=new Scanner(System.in)
+a=s.nextInt();
+b=s.nextInt();
+{
+a=a+b;
+}
+System.out.println(a);
+}
+}
