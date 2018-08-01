b229e397e699791f7fa018d168a1edc0222701ad
15ec45 <priyasengathir19@gmail.com>
2018-08-01 16:29:08 +0530

    Create sum.java

---
+++ sum of no/sum.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class sum
+{
+public static void main(String args[])
+{
+int a,b,c,d,e,n;
+Scanner s=new Scanner(System.in);
+n=s.nextInt();
+a=n/10;
+b=n%10;
+c=a/10;
+d=a%10;
+e=b+c+d;
+{
+System.out.println(e);
+}
+}
+}
