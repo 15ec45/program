75bdf1243eaba22a857701c3e047d4959b52c106
15ec45 <priyasengathir19@gmail.com>
2018-08-01 15:49:39 +0530

    Create power.java

---
+++ power/power.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class power
+{
+public static void main(String []args)
+{
+int a;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+if(a%2==0)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
