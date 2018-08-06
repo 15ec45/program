d044dfd761650bf6345e6da28497ea608b7a96b1
15ec45 <priyasengathir19@gmail.com>
2018-08-06 15:48:59 +0530

    Create multi.java

---
+++ multi/multi.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class multi
+{
+public static void main(String []args)
+{
+int a;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+if(a%13==0)
+{
+System.out.println("yes");
+}
+else
+{
+System.out.println("no");
+}
+}
+}
