0003bd9c09bbc1b3127d58f1dbf89a01ffb91595
15ec45 <priyasengathir19@gmail.com>
2018-08-06 15:58:26 +0530

    Create digitl.java

---
+++ digit/digitl.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+public class digit
+{
+public static void main(String []args)
+{
+int a,b,c,d;
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=a/10;
+c=a%10;
+d=(b*b)+(c*c)
+{
+System.out.println(d);
+}
+}
+}
