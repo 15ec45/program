d9635df2c5e842936bbb565392cac99ea47a98b3
15ec45 <priyasengathir19@gmail.com>
2018-08-23 15:09:54 +0530

    Create StringReverse.java

---
+++ StringReverse/StringReverse.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class StringReverse
+{
+public static void main(String[] args)
+{
+String a;
+Scanner s=new Scanner(System.in);
+a=s.nextLine();
+String reverse=new StringBuffer(a).reverse,toString();
+System.out.println(reverse);
+}
+}
